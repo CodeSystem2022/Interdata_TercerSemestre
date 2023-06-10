c30697674d295f5935ac0ab015e315c52896feb5
ignacio buchter <111471268+Naachooo@users.noreply.github.com>
2023-06-09 23:40:57 -0300

    agregue actualizar varios registros

---
+++ Semana  4/Python/actualizar_varios_registros.py
@@ -0,0 +1,19 @@
+import psycopg2 # Esto es para conectarnos a postgres
+
+conexion = psycopg2.connect(user='postgres', password='admin', host='127.0.0.1', port='5432', database='test_bd')
+try:
+    with conexion:
+        with conexion.cursor() as cursor:
+            sentencia = 'UPDATE persona SET nombre=%s, apellido=%s, email=%s WHERE id_persona=%s'
+            valores = (
+                ('Joaquin', 'Halle', 'hallejoaquin@gmail.com', 8),
+                ('Elena', 'Bekasenas', 'elenabekasenas@gmail.com', 5)
+            )  # es una tupla de tuplas
+            cursor.executemany(sentencia, valores)  # De esta manera ejecutamos la sentencia
+            registros_actualizados = cursor.rowcount
+            print(f'Los registros actualizados son: {registros_actualizados}')
+
+except Exception as e:
+    print(f'Ocurrio un error: {e}')
+finally:
+    conexion.close()
