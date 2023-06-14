5974e434383826ba7833ca01c2a4bd504c57b299
Francisco Chahla <114193407+Frant007@users.noreply.github.com>
2023-06-13 23:10:00 -0300

    Create FiguraGeometrica.java

---
+++ Semana  4/Java/ClasesAbstractas/FiguraGeometrica.java
@@ -0,0 +1,24 @@
+package domain;
+
+public abstract class FiguraGeometrica {
+    protected String tipoFigura;
+    protected FiguraGeometrica(String tipoFigura){
+        this.tipoFigura = tipoFigura;
+    }
+    //Metodo Abstracto
+    public abstract void dibujar();
+
+    //Agregamos set and get
+    public String getTipoFigura() {
+        return tipoFigura;
+    }
+
+    public void setTipoFigura(String tipoFigura) {
+        this.tipoFigura = tipoFigura;
+    }
+
+    @Override
+    public String toString() {
+        return "FiguraGeometrica{" + "tipoFigura=" + tipoFigura + "}";
+    }
+}
