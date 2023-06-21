52c88e9a5afeab525c981ed8aa115b1e823878d9
ignacio buchter <111471268+Naachooo@users.noreply.github.com>
2023-06-21 20:55:22 -0300

    transacciones parte 3 y 4

--- Semana 5/Python/transacciones_2.py
+++ Semana 5/Python/transacciones_2.py
@@ -1,19 +1,24 @@
-import psycopg2 as bd
+import psycopg2 as bd # Esto es para conectarnos a postgres
 
 conexion = bd.connect(user='postgres', password='admin', host='127.0.0.1', port='5432', database='test_bd')
 try:
-    #   conexion.autocommit = False   # esto no deberia estar, se inica la transaccion
+    conexion.autocommit = False # esto directamente no deberia estar, inicia la transaccion
     cursor = conexion.cursor()
-    sentencia = 'INSERT INTO persona(nombre, apellido, email)VALUES (%s, %s, %s)'
-    valores = ('Silvina', 'Prado', 'spppp@gmail.com')
+    sentencia = 'INSERT INTO persona(nombre, apellido, email) VALUES(%s, %s, %s)'
+    valores = ('Clara', 'Esparza', 'cesparza@mail.com')
     cursor.execute(sentencia, valores)
+
     sentencia = 'UPDATE persona SET nombre=%s, apellido=%s, email=%s WHERE id_persona=%s'
-    valores = ('Juan', 'Etes', 'jetes@gmail.com', 2)
+    valores = ('Juan', 'Juarez', 'jcjuarez@mail.com', 13)
     cursor.execute(sentencia, valores)
-    conexion.commit()  # se hace el commit manualmente   # aqui se cierra la transaccion
-    print('Termina la transaccion')
+
+    conexion.commit() # con el commit manual cerramos la transaccion
+
+    print('termina la transaccion')
+    conexion.commit()  # hacemos el commit manualmente
+
 except Exception as e:
-    conexion.rollback()  # en caso de que el commit no se haga por x error, se hace un rollback..
-    print(f'Ocurrio un error: {e}')  # ..y no se guardan los cambios
+    conexion.rollback()
+    print(f'Ocurrio un error, se hizo un rollback: {e}')
 finally:
     conexion.close()
