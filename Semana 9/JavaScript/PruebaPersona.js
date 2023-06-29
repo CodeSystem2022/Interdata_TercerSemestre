fc788c5a180aaedaf5e9734857a9dc020c917244
GabrielEchave <112594225+GabrielEchave@users.noreply.github.com>
2023-06-29 01:08:47 -0300

    Update PruebaPersona.js

--- Semana 9/JavaScript/PruebaPersona.js
+++ Semana 9/JavaScript/PruebaPersona.js
@@ -103,3 +103,24 @@ class Cliente extends Persona{
         ${this._fechaRegistro}`;
     }
 }
+
+//Prueba clase Persona
+let persona1 = new Persona('Juan', 'Perez', 32);
+console.log(persona1.toString());
+
+let persona2 = new Persona('Carla', 'Ortega', 22);
+console.log(persona2.toString());
+
+// Prueba clase Empleado
+let empleado1 = new Empleado('Pedro', 'Roman' , 18, 5000);
+console.log(empleado1.toString());
+
+let empleado2 = new Empleado('Jonas', 'Torres',30 , 7000);
+console.log(empleado2.toString());
+
+// Prueba clase Cliente
+let cliente1 = new Cliente('Miguel', 'Zala' , 29, new Date());
+console.log(cliente1.toString());
+
+let cliente2 = new Cliente('Natalia', 'Ortega', 22 , new Date());
+console.log(cliente2.toString());
