8bf55cab4aef1ba14815895d526285e8617734a4
Silvina Prado <112594123+SilPrad0@users.noreply.github.com>
2023-06-29 10:18:28 -0300

    Capa de datos, videos 1 y 2

---
+++ Semana 8/Python/Persona1y2.py
@@ -0,0 +1,16 @@
+# definimos los metodos de clase
+    @classmethod
+    def seleccionar(cls):
+        with Conexion.obtenerConexion():
+            with Conexion.obtenerCursor() as cursor:
+                cursor.execute(cls._SELECCIONAR)
+                registros = cursor.fetchall()
+                personas = []
+                for registro in registros:
+                    persona = Persona(registro[0], registro[1], registro[2], registro[3])
+                    personas.append(persona)
+                return personas
+if __name__ == '__main__':
+    personas = PersonaDAO.seleccionar()
+      for persona in personas:
+          logging.debug(persona)
